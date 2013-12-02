c0f80cb800620f03984ba07b9d1c40a249dd31ee
Sam Hamilton <shamilton@pragmatrading.com>
2013-12-02 06:30:41 -0500

    cleanup

--- out/production/bouncer/main.go
+++ out/production/bouncer/main.go
@@ -2,16 +2,71 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"github.com/gorilla/mux"
-
+	"log"
+	"encoding/json"
+	"os"
+	"flag"
+	"github.com/samilton/bouncer/engine"
 )
 
-func main() {
-	fmt.Println("Starting our Web Server")
-	r := mux.NewRouter()
+type webhook struct {
+	After string `json:"after"`
+	HeadCommit headCommit `json:"head_commit"`
+	Repository repo `json:"repository"`
+}
 
+type headCommit struct {
+	Added []string `json:"added"`
+	Modified []string `json:"modified"`
+	Removed []string `json:"removed"`
+	Author author `json:"author"`
+	Committer committer `json:"committer"`
+	Message string `json:"message"`
+}
+
+type author struct {
+	Email string `json:"email"`
+	Name string `json:"name"`
+	UserName string `json:"username"`
+}
+
+type committer struct {
+	Email string `json:"email"`
+	Name string `json:"name"`
+	UserName string `json:"username"`
+}
+
+type repo struct {
+	CreatedAt int64 `json:"created_at"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	MasterBranch string `json:"master_branch"`
+	Url string `json:"url"`
+}
+
+
+func main() {
+
+	c := make(chan string, 1)
+	go func () { fmt.Println("Got %s from the channel", &lt;- c) }()
+	c &lt;- "foobar"
+
+
+	var configFile string
+	flag.StringVar(&configFile, "configFile", "bouncer.json", "Configuration File")
+	flag.Parse()
+
+	config, err := os.Open(configFile)
+
+	log.Println("Starting Web Bouncer Daemon")
+	if err == nil {
+		decoder := json.NewDecoder(config)
+		var configuration engine.Configuration
+		decoder.Decode(&configuration)
+		engine.Start(&configuration)
+	} else {
+		panic(err)
+	}
 }
 
 
